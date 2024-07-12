2a9814c0fa0c812a7a950154ed108d03851fc595
p1ka <lilianmorel42@yahoo.fr>
2024-07-12 23:04:02 +0200

    moved login on profile page, reorganized pages

--- back/app/views.py
+++ back/app/views.py
@@ -12,10 +12,11 @@ logger = logging.getLogger(__name__)
 
 # Create your views here.
 
+@login_required
 def logout_user(request):
     print("LOGOUT", request.user, file=sys.stderr)
     logout(request)
-    return (redirect('home'))
+    return (redirect('myprofile'))
 
 def homeView(request):
     next_url = get_url('home')
@@ -38,12 +39,16 @@ def homeView(request):
         return render(request, 'page_full.html', {'page':'home.html', 'form':form, 'next_url':next_url})
     return render(request, 'home.html', {'form':form, 'next_url':next_url})
 
-@login_required
+
 def gameView(request):
-    user = get_object_or_404(User, id=request.user.id)
-    if request.META.get("HTTP_HX_REQUEST") != 'true':
-        return render(request, 'page_full.html', {'page':'game.html', 'user':user})
-    return render(request, 'game.html', {'user':user})
+    if request.user.is_authenticated:
+        if request.META.get("HTTP_HX_REQUEST") != 'true':
+            return render(request, 'page_full.html', {'page':'game.html', 'user':request.user})
+        return render(request, 'game.html', {'user':request.user})
+    else:
+        if request.META.get("HTTP_HX_REQUEST") != 'true':
+            return render(request, 'page_full.html', {'page':'game.html'})
+        return render(request, 'game.html')
 
 def registrationView(request):
     if request.method == 'POST':
@@ -58,7 +63,7 @@ def registrationView(request):
             user.save()
             # auto-login user
             login(request, user)
-            return redirect('home')
+            return redirect('myprofile')
         else:
             logger.warning(f"Form errors: {form.errors}")
     else:
@@ -67,12 +72,32 @@ def registrationView(request):
         return render(request, 'page_full.html', {'page':'registration.html', 'form':form})
     return render(request, 'registration.html', {'form': form})
 
-@login_required
 def myProfilView(request):
-    user = get_object_or_404(User, id=request.user.id)
+
+    if request.user.is_authenticated:
+        if request.META.get("HTTP_HX_REQUEST") != 'true':
+            return render(request, 'page_full.html', {'page':'myprofil.html', 'user':request.user})
+        return render(request, 'myprofil.html', {'user':request.user})
+
+    next_url = get_url('myprofile')
+    if (request.GET.get('next')):
+        next_url = request.GET.get('next')
+    form = LoginForm()
+
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+            )
+            if user is not None:
+                print("login", request.user, file=sys.stderr)
+                login(request, user)
+                return (redirect(next_url))
     if request.META.get("HTTP_HX_REQUEST") != 'true':
-        return render(request, 'page_full.html', {'page':'myprofil.html', 'user':user})
-    return render(request, 'myprofil.html', {'user':user})
+        return render(request, 'page_full.html', {'page':'myprofil.html', 'form':form, 'next_url':next_url})
+    return render(request, 'myprofil.html', {'form':form, 'next_url':next_url})
 
 def profilView(request, username):
     user = get_object_or_404(User, username=username)
