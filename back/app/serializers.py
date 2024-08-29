5022c4adc0ff5c535dcc65fe86022a8e091f1ec5
LiesXV <lies.benh@gmail.com>
2024-08-29 15:20:34 +0200

    upgrade game, adjust endpoint in view to automatize the API

--- back/app/serializers.py
+++ back/app/serializers.py
@@ -1,9 +1,23 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
-
-from app.models import Game
+from app.models import Game, Pong
+from django.contrib.auth.models import User
 
 class GameSerializer(ModelSerializer):
+    player1 = serializers.CharField(source='player1.username')
+    player2 = serializers.CharField(source='player2.username')
+    pong = serializers.SerializerMethodField()
 
-	class Meta:
-		model = Game
-		fields = '__all__'
+    class Meta:
+        model = Game
+        fields = ['id', 'gametype', 'player1', 'player2', 'pong']
+
+    def get_pong(self, instance):
+        queryset = Pong.objects.all()
+        serializer = PongSerializer(queryset, many=True)
+        return serializer.data
+
+class PongSerializer(ModelSerializer):
+    class Meta:
+        model = Pong
+        fields = '__all__'
