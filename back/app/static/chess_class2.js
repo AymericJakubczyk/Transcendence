bc82e9f80df2e2abf505173797978ec0b6fa0f26
ajakubcz <aymeric.jaku@gmail.com>
2024-10-25 17:06:03 +0200

    chessV2

---
+++ back/app/static/chess_class2.js
@@ -0,0 +1,225 @@
+class Cell
+{
+    constructor()
+    {
+        this.piece = null;
+        this.possibleMove = false;
+        this.move = false;
+        this.enPassant = false;
+        this.castling = false; // roque
+    }
+}
+
+class Pawn {
+    constructor (color)
+    {
+        this.color = color;
+        this.src = "/static/srcs/chess/" + color + "pawn.svg";
+    }
+    
+    setPossibleMoves(board, x, y)
+    {
+        if (this.color == 'white')
+        {
+            if (y == 6 && board[y-1][x].piece == null && board[y-2][x].piece == null)
+            {
+                board[y-2][x].possibleMove = true;
+                board[y-1][x].enPassant = true;
+            }
+
+            if (board[y-1][x].piece == null)
+                board[y-1][x].possibleMove = true;
+            if (y - 1 >= 0 && x + 1 < 8 && board[y-1][x+1].piece != null && board[y-1][x+1].piece.color != this.color)
+                board[y-1][x+1].possibleMove = true;
+            if (y - 1 >= 0 && x - 1 >= 0 && board[y-1][x-1].piece != null && board[y-1][x-1].piece.color != this.color)
+                board[y-1][x-1].possibleMove = true;
+            if (y - 1 == 2 && x + 1 < 8 && board[y-1][x+1].enPassant)
+                board[y-1][x+1].possibleMove = true;
+            if (y - 1 == 2 && x - 1 >= 0 && board[y-1][x-1].enPassant)
+                board[y-1][x-1].possibleMove = true;
+        }
+        else if (this.color == 'black')
+        {
+            if (y == 1 && board[y+1][x].piece == null && board[y+2][x].piece == null)
+            {
+                board[y+2][x].possibleMove = true;
+                board[y+1][x].enPassant = true;
+            }
+
+            if (board[y+1][x].piece == null)
+                board[y+1][x].possibleMove = true;
+            if (y + 1 < 8 && x + 1 < 8 && board[y+1][x+1].piece != null && board[y+1][x+1].piece.color != this.color)
+                board[y+1][x+1].possibleMove = true;
+            if (y + 1 < 8 && x - 1 >= 0 && board[y+1][x-1].piece != null && board[y+1][x-1].piece.color != this.color)
+                board[y+1][x-1].possibleMove = true;
+            if (y + 1 == 5 && x + 1 < 8 && board[y+1][x+1].enPassant)
+                board[y+1][x+1].possibleMove = true;
+            if (y + 1 == 5 && x - 1 >= 0 && board[y+1][x-1].enPassant)
+                board[y+1][x-1].possibleMove = true;
+        }
+    }
+}
+
+class Knight {
+    constructor (color)
+    {
+        this.color = color;
+        this.src = "/static/srcs/chess/" + color + "knight.svg";
+    }
+    
+    setPossibleMoves(board, x, y)
+    {
+        if (x - 1 >= 0 && y - 2 >= 0 && (board[y-2][x-1].piece == null || (board[y-2][x-1].piece && board[y-2][x-1].piece.color != this.color)))
+            board[y-2][x-1].possibleMove = true;
+        if (x + 1 <= 7 && y - 2 >= 0 && (board[y-2][x+1].piece == null || (board[y-2][x+1].piece && board[y-2][x+1].piece.color != this.color)))
+            board[y-2][x+1].possibleMove = true;
+        if (x - 2 >= 0 && y - 1 >= 0 && (board[y-1][x-2].piece == null || (board[y-1][x-2].piece && board[y-1][x-2].piece.color != this.color)))
+            board[y-1][x-2].possibleMove = true;
+        if (x + 2 <= 7 && y - 1 >= 0 && (board[y-1][x+2].piece == null || (board[y-1][x+2].piece && board[y-1][x+2].piece.color != this.color)))
+            board[y-1][x+2].possibleMove = true;
+        if (x - 2 >= 0 && y + 1 <= 7 && (board[y+1][x-2].piece == null || (board[y+1][x-2].piece && board[y+1][x-2].piece.color != this.color)))
+            board[y+1][x-2].possibleMove = true;
+        if (x + 2 <= 7 && y + 1 <= 7 && (board[y+1][x+2].piece == null || (board[y+1][x+2].piece && board[y+1][x+2].piece.color != this.color)))
+            board[y+1][x+2].possibleMove = true;
+        if (x - 1 >= 0 && y + 2 <= 7 && (board[y+2][x-1].piece == null || (board[y+2][x-1].piece && board[y+2][x-1].piece.color != this.color)))
+            board[y+2][x-1].possibleMove = true;
+        if (x + 1 <= 7 && y + 2 <= 7 && (board[y+2][x+1].piece == null || (board[y+2][x+1].piece && board[y+2][x+1].piece.color != this.color)))
+            board[y+2][x+1].possibleMove = true;
+    }
+}
+
+class Rook {
+    constructor (color)
+    {
+        this.color = color;
+        this.src = "/static/srcs/chess/" + color + "rook.svg";
+    }
+    
+    setPossibleMoves(board, x, y)
+    {
+        let piecePos = {'x': x, 'y': y};
+        for (let i = x + 1; i < 8; i++)
+        {
+            if (verif_loop_moove(board, this.color, i, y))
+                break;  
+        }
+        for (let i = x - 1; i >= 0; i--)
+        {
+            if (verif_loop_moove(board, this.color, i, y))
+                break;
+        }
+        for (let j = y + 1; j < 8; j++)
+        {
+            if (verif_loop_moove(board, this.color, x, j))
+                break;
+        }
+        for (let j = y - 1; j >= 0; j--)
+        {
+            if (verif_loop_moove(board, this.color, x, j))
+                break;
+        }
+    }
+}
+
+class Bishop {
+    constructor (color)
+    {
+        this.color = color;
+        this.src = "/static/srcs/chess/" + color + "bishop.svg";
+    }
+    
+    setPossibleMoves(board, x, y)
+    {
+        for (let i = x + 1, j = y + 1; i < 8 && j < 8; i++, j++)
+        {
+            if (verif_loop_moove(board, this.color, i, j))
+                break;
+        }
+        for (let i = x - 1, j = y - 1; i >= 0 && j >= 0; i--, j--)
+        {
+            if (verif_loop_moove(board, this.color, i, j))
+                break;
+        }
+        for (let i = x + 1, j = y - 1; i < 8 && j >= 0; i++, j--)
+        {
+            if (verif_loop_moove(board, this.color, i, j))
+                break;
+        }
+        for (let i = x - 1, j = y + 1; i >= 0 && j < 8; i--, j++)
+        {
+            if (verif_loop_moove(board, this.color, i, j))
+                break;
+        }    
+    }
+}
+
+class King {
+    constructor (color)
+    {
+        this.color = color;
+        this.src = "/static/srcs/chess/" + color + "king.svg";
+    }
+    
+    setPossibleMoves(board, x, y)
+    {
+        if (x - 1 >= 0 && (board[y][x-1].piece == null || board[y][x-1].piece.color != this.color))
+            board[y][x-1].possibleMove = true;
+        if (x + 1 <= 7 && (board[y][x+1].piece == null || board[y][x+1].piece.color != this.color))
+            board[y][x+1].possibleMove = true;
+        if (y - 1 >= 0 && (board[y-1][x].piece == null || board[y-1][x].piece.color != this.color))
+            board[y-1][x].possibleMove = true;
+        if (y + 1 <= 7 && (board[y+1][x].piece == null || board[y+1][x].piece.color != this.color))
+            board[y+1][x].possibleMove = true;
+        if (x - 1 >= 0 && y - 1 >= 0 && (board[y-1][x-1].piece == null || board[y-1][x-1].piece.color != this.color))
+            board[y-1][x-1].possibleMove = true;
+        if (x + 1 <= 7 && y - 1 >= 0 && (board[y-1][x+1].piece == null || board[y-1][x+1].piece.color != this.color))
+            board[y-1][x+1].possibleMove = true;
+        if (x - 1 >= 0 && y + 1 <= 7 && (board[y+1][x-1].piece == null || board[y+1][x-1].piece.color != this.color))
+            board[y+1][x-1].possibleMove = true;
+        if (x + 1 <= 7 && y + 1 <= 7 && (board[y+1][x+1].piece == null || board[y+1][x+1].piece.color != this.color))
+            board[y+1][x+1].possibleMove = true;
+        
+        // Castling (roque)
+        if (!board[y][x].move && !board[y][0].move && board[y][1].piece == null && board[y][2].piece == null && board[y][3].piece == null)
+        {
+            board[y][2].possibleMove = true;
+            board[y][2].castling = true;
+        }
+        if (!board[y][x].move && !board[y][7].move && board[y][5].piece == null && board[y][6].piece == null)
+        {
+            board[y][6].possibleMove = true;
+            board[y][6].castling = true;
+        }
+    }
+}
+
+class Queen {
+    constructor (color)
+    {
+        this.color = color;
+        this.src = "/static/srcs/chess/" + color + "queen.svg";
+    }
+    
+    setPossibleMoves(board, x, y)
+    {
+        new Rook(this.color).setPossibleMoves(board, x, y);
+        new Bishop(this.color).setPossibleMoves(board, x, y);
+    }
+}
+
+
+function verif_loop_moove(board, my_color, x, y)
+{
+    if (board[y][x].piece == null)
+    {
+        board[y][x].possibleMove = true;
+        return 0;
+    }
+    else if (board[y][x].piece.color != my_color)
+    {
+        board[y][x].possibleMove = true;
+        return 1;
+    }
+    else
+        return 1;
+}
---
+++ back/app/static/chess_utils.js
@@ -0,0 +1,123 @@
+function reset_possible_moves(board)
+{
+    let allmove = document.getElementsByClassName("move");
+    for (let i = allmove.length - 1; i >= 0; i--)
+        allmove[i].remove();
+
+    for (let i = 0; i < 8; i++)
+    {
+        for (let j = 0; j < 8; j++)
+        {
+            board[i][j].possibleMove = false;
+        }
+    }
+}
+
+function reset_possible_castling(board)
+{
+    board[0][2].castling = false;
+    board[0][6].castling = false;
+    board[7][2].castling = false;
+    board[7][6].castling = false;
+}
+
+function last_verif_move(board, color, piecePos)
+{
+    for (let y = 0; y < 8; y++)
+    {
+        for (let x = 0; x < 8; x++)
+        {
+            if (board[y][x].possibleMove)
+            {
+                let cp_board = Array.from(board, x => x.map(y => Object.assign({}, y)));
+                cp_board[y][x].piece = cp_board[piecePos.y][piecePos.x].piece;
+                cp_board[piecePos.y][piecePos.x].piece = null;
+                reset_possible_moves(cp_board);
+                if (verif_check(cp_board, color))
+                    board[y][x].possibleMove = false;
+                if (board[y][x].castling)
+                {
+                    if (x == 2 && !bigCastling(board, x, y))
+                    {
+                        board[y][x].possibleMove = false;
+                        board[y][x].castling = false;
+                    }
+                    if (x == 6 && !smallCastling(board, x, y))
+                    {
+                        board[y][x].possibleMove = false;
+                        board[y][x].castling = false;
+                    }
+                }
+            }
+
+        }
+    }
+}
+
+function verif_check(cp_board, color)
+{
+    let kingPos = getKingPos(cp_board, color);
+    for (let y = 0; y < 8; y++)
+    {
+        for (let x = 0; x < 8; x++)
+        {
+            if (cp_board[y][x].piece && cp_board[y][x].piece.color != color)
+            {
+                cp_board[y][x].piece.setPossibleMoves(cp_board, x, y);
+                if (cp_board[kingPos.y][kingPos.x].possibleMove)
+                {
+                    console.log("check", x, y, kingPos.x, kingPos.y);
+                    return true;
+                }
+            }
+        }
+    }
+    return false
+
+}
+
+function getKingPos(board, color)
+{
+    for (let y = 0; y < 8; y++)
+    {
+        for (let x = 0; x < 8; x++)
+        {
+            if (board[y][x].piece && board[y][x].piece.color == color && board[y][x].piece.constructor.name == "King")
+            {
+                console.log("king", x, y);
+                return ({'x': x, 'y': y});
+            }
+        }
+    }
+    return ({'x': 0, 'y': 0});
+}
+
+function remove_en_passant(board, color)
+{
+    let y = 0;
+    if (color == "white")
+        y = 2;
+    else
+        y = 5;
+    for (let x = 0; x < 8; x++)
+    {
+        console.log("remove en passant",board[y][x].enPassant, x, y);
+        board[y][x].enPassant = false;
+    }
+}
+
+function bigCastling(board, x, y)
+{
+    let cp_board = Array.from(board, x => x.map(y => Object.assign({}, y)));
+    if (verif_check(cp_board, board[y][4].piece.color) || !board[y][3].possibleMove)
+        return false
+    return true
+}
+
+function smallCastling(board, x, y)
+{
+    let cp_board = Array.from(board, x => x.map(y => Object.assign({}, y)));
+    if (verif_check(cp_board, board[y][4].piece.color) || !board[y][5].possibleMove)
+        return false
+    return true
+}
